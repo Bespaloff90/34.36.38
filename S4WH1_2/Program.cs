11dbb174c5f81ede2dfd9b6168e6d2b2e7d07757
Andrey_BespaloFF <andrey2802@mail.ru>
2023-03-11 11:12:05 +0300

    34 random number elements

---
+++ S4WH1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+Console.WriteLine("Hello friend! ");
+int[] arr = new int[12];
+            int k = 0; //sum = 0;
+            Random rnd = new Random();
+ 
+            Console.WriteLine("Input array");
+            for (int i = 0; i < 12; i++)
+            {
+                arr[i] = rnd.Next(99, 1000);
+                Console.Write("{0} ", arr[i]);
+            }
+             Console.WriteLine();
+            Console.WriteLine("Array in ascending order ");
+             Array.Sort(arr);
+ 
+            foreach(int element in arr)
+             {
+                 Console.Write("{0} ", element);
+               if (element % 2 == 0)
+                {
+                     k++;
+                   
+                 }
+           }
+             Console.WriteLine();
+             Console.WriteLine("Number of even elements:  {0}", k); 
