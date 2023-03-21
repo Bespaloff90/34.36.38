804949132dd1de77398a5e0e92b00de92b3cfb12
Andrey_BespaloFF <andrey2802@mail.ru>
2023-03-21 15:23:46 +0300

    27

---
+++ S4WH27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("This program allows you to find the sum of the entered numbers");
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum of numbers = {GetSumNums(num)}");
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0) 
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
